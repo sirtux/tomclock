ce0e7ee9473354971969fc6fa2c144305d2a7ab3
Tom Eichhorn <tom@wirkbetrieb.net>
2023-05-01 08:23:26 +0200

    MicroPython Code to run the thing

---
+++ software/update_time.py
@@ -0,0 +1,13 @@
+import serial
+import datetime
+
+current_utc = datetime.datetime.utcnow()
+ser = serial.Serial("/dev/tty.usbmodem2111301", 115200, timeout=1)
+
+timestring = f"{current_utc.year}, {current_utc.month}, {current_utc.day}, 0, {current_utc.hour}, {current_utc.minute}, {current_utc.second}"
+print(timestring)
+ser.write(bytes(timestring, encoding="utf-8"))
+ser.write("\n".encode())
+print(ser.readlines())
+
+ser.close()
