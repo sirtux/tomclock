65ddbb29e96db73e0530b9b84bff4d1b9094342c
Tom Eichhorn <tom@wirkbetrieb.net>
2023-05-15 18:50:54 +0200

    Change to timer interrupt

--- software/update_time.py
+++ software/update_time.py
@@ -7,7 +7,7 @@ ser = serial.Serial("/dev/tty.usbmodem2111301", 115200, timeout=1)
 timestring = f"{current_utc.year}, {current_utc.month}, {current_utc.day}, 0, {current_utc.hour}, {current_utc.minute}, {current_utc.second}"
 print(timestring)
 ser.write(bytes(timestring, encoding="utf-8"))
-ser.write("\n".encode())
+ser.write("\r\n".encode())
 print(ser.readlines())
 
 ser.close()
